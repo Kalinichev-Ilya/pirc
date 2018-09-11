67284e37c7bee991f83a1d73f3903f42fdd8f68f
ikzekly <iluya.kalinichev@gmail.com>
2018-09-12 00:39:49 +0300

    [0.7] add swagger doc for grape api

---
+++ src/app/api/root.rb
@@ -0,0 +1,21 @@
+class Root < Grape::API
+  prefix :api
+  version :v1, using: :path
+
+  format :json
+
+  mount V1::Base
+
+  add_swagger_documentation(
+      add_version:             true,
+      doc_version:             '0.0.1',
+      hide_documentation_path: true,
+      info:                    {
+          title: 'pIRC API V1'
+      }
+  )
+
+  route :any, '*path' do
+    raise Errors::NotFound, "No such route '#{request.path}'"
+  end
+end
--- src/config/application.rb
+++ src/config/application.rb
@@ -38,5 +38,17 @@ module Pirc
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
+
+    # Grape API
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
+
+    # Rack Cors
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: %i[get post options]
+      end
+    end
   end
 end
