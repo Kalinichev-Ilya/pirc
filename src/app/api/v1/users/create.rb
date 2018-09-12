4dcca94971a12d836dc7362f4c454f25f080e846
ikzekly <iluya.kalinichev@gmail.com>
2018-09-13 01:39:18 +0300

    add create user endpoint

--- src/app/api/v1/users/create.rb
+++ src/app/api/v1/users/create.rb
@@ -1,19 +1,21 @@
+# frozen_string_literal: true
+
 module V1
   module Users
-    class Create
-      version :v1 do
+    class Create < Grape::API
+      namespace :users do
         # POST /api/v1/users
         desc 'Create new user account',
-             named: 'create',
-             success: { code: 201, model: API::V1::Entities::User}
+          named:   'create',
+          success: { code: 201, model: V1::Entities::User }
         params do
-          requires :username, type: String, allow_blanck: false
-          requires :password_digest, type: String, allow_blanck: false
+          requires :username, type: String
+          requires :password_digest, type: String
         end
         post do
           attributes = declared(params).to_h
           present ::User.create!(attributes).reload,
-                  with: API::V1::Entities::User
+            with: V1::Entities::User
         end
       end
     end
