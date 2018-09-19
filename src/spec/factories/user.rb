e19ca5012ca199b8282077486f934f63b22d86d1
ikzekly <iluya.kalinichev@gmail.com>
2018-09-20 00:49:17 +0300

    [0.9.3] fix user factory

--- src/spec/factories/user.rb
+++ src/spec/factories/user.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :user, aliases: [:owner] do
     username { Faker::Internet.user_name(4..15) }
-    password { Faker::Internet.password(8, 18) + '!!' }
+    password { Faker::Internet.password(8, 18, true, true) }
 
     trait :invalid do
       username { 'min' }
