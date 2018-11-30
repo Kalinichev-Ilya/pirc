7f7fa77a9f00fb2db8a39d0b2c84aeac314f1898
ikzekly <iluya.kalinichev@gmail.com>
2018-11-30 13:17:49 +0300

    [0.10.1] add auth#refresh specs

--- src/spec/api/v1/auth/refresh_spec.rb
+++ src/spec/api/v1/auth/refresh_spec.rb
@@ -10,7 +10,14 @@ RSpec.describe API::V1::Auth::Refresh do
   end
 
   describe 'POST /api/v1/auth/refresh' do
-    let(:access_token) { create(:access_token) }
+    let(:access_token) do
+      create(
+        :access_token,
+        essence_hash: Digest::SHA2.hexdigest('valid_essence'),
+        refresh_token_hash: Digest::SHA2.hexdigest('valid_refresh_token'),
+        expires_at: Time.current + 1.hour
+      )
+    end
     let(:params) { { refresh_token: 'valid_refresh_token' } }
 
     context 'valid' do
@@ -19,6 +26,24 @@ RSpec.describe API::V1::Auth::Refresh do
         post '/api/v1/auth/refresh', params
 
         expect(last_response.status).to eq(201)
+      end
+    end
+
+    context 'fail' do
+      let(:params) { { refresh_token: 'wrong_refresh_token' } }
+
+      it 'with wrong Access token returns code 403' do
+        header 'X-Auth-Token', 'wrong_essence'
+        post '/api/v1/auth/refresh', params
+
+        expect(last_response.status).to eq(403)
+      end
+
+      it 'with wrong refresh token returns code 403' do
+        header 'X-Auth-Token', 'valid_essence'
+        post '/api/v1/auth/refresh', params
+
+        expect(last_response.status).to eq(403)
       end
     end
   end
--- src/spec/factories/access_token.rb
+++ src/spec/factories/access_token.rb
@@ -3,10 +3,6 @@
 FactoryBot.define do
   factory :access_token do
     user
-
-    essence_hash { Digest::SHA2.hexdigest('valid_essence') }
-    refresh_token_hash { Digest::SHA2.hexdigest('valid_refresh_token') }
-    expires_at { Time.current + 1.hour }
   end
 
   trait :expired do
