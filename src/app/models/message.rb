d3c25b23412a8dd8a147d03b14569d04577a38df
ikzekly <iluya.kalinichev@gmail.com>
2018-12-04 02:10:28 +0300

    [0.13.1] add operation for create message

--- src/app/models/message.rb
+++ src/app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :channel
 
-  validates :user_id, :channel_id, :message_type, :text, presence: true
+  validates :user_id, :channel_id, :message_type, presence: true
   validates :message_type, inclusion: { in: %w[plain invite] }
 end
